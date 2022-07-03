f9f947464483735d50643781561b0bceec335bc4
DmitryPatskalev <dimanord@yandex.ru>
2022-07-03 22:32:44 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import './styles/App.css'
+import PostsList from "./component/PostsList";
+import PostForm from "./component/PostForm";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaScript', body: 'description'},
+    {id: 2, title: 'JavaScript 2', body: 'description'},
+    {id: 3, title: 'JavaScript 3', body: 'description'},
+  ])
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
 
 
   return (
     <div className="App">
-      <div className='post'>
-        <div className="post_content">
-          <strong>1. JavaScript</strong>
-          <div>
-            JavaScript - язык программирования.
-          </div>
-        </div>
-        <div className="post_btns">
-          <button>remove</button>
-        </div>
-      </div>
+      <PostForm create={createPost}/>
+      {posts.length?
+        <PostsList remove={removePost} posts={posts} title='List of posts JS'/>:
+        <h1 style={{textAlign:'center', color:'green'}}>Please, create post!</h1>
+      }
+
     </div>
   );
 }
---
+++ src/component/PostItem.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import MyButton from "./UI/button/MyButton";
+
+const PostItem = (props) => {
+  const removePost = ()=>{
+    props.remove(props.post)
+  }
+
+
+  return (
+    <div className='post'>
+      <div className="post_content">
+        <strong>{props.number} {props.post.title}</strong>
+        <div>
+          {props.post.body}
+        </div>
+      </div>
+      <div className="post_btns">
+        <MyButton onClick={removePost}>remove</MyButton>
+      </div>
+    </div>
+  );
+};
+
+export default PostItem;
